293b00f7e18b8eec380889547462b15c540246d6
bash <yesufubashir@gmail.com>
2022-06-15 23:24:52 +0100

    Create project

---
+++ Mintyn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mintyn
+//
+//  Created by Bash on 15/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
