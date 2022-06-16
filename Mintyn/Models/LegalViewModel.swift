46dd5649fcccdda37292652f9eb6c6edc42d0e19
bash <yesufubashir@gmail.com>
2022-06-16 18:55:44 +0100

    Add legal view model

---
+++ Mintyn/Models/LegalViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LegalViewModel.swift
+//  Mintyn
+//
+//  Created by Bash on 16/06/2022.
+//
+
+import Foundation
+
+struct LegalViewModel {
+    let title: String
+    let itemURL: String
+}
