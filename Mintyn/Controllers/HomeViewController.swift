6421dc823abc65107d4a62953f19bd61e84a457f
bash <yesufubashir@gmail.com>
2022-06-17 09:11:41 +0100

    integrate home view toggle feature

--- Mintyn/Controllers/HomeViewController.swift
+++ Mintyn/Controllers/HomeViewController.swift
@@ -9,21 +9,25 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var transparentBackground: UIView!
+    @IBOutlet weak var optionsView: UIStackView!
+    @IBOutlet weak var floatingActionView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupFAB()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupFAB() {
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(self.toggleOptionsVisibility))
+        floatingActionView.addGestureRecognizer(gesture)
     }
-    */
+    
+    @objc private func toggleOptionsVisibility() {
+        transparentBackground.isHidden = !transparentBackground.isHidden
+        optionsView.isHidden = !optionsView.isHidden
+        floatingActionView.image = optionsView.isHidden ? UIImage(systemName: "plus.circle.fill") : UIImage(systemName: "xmark.app.fill")
+    }
 
 }
